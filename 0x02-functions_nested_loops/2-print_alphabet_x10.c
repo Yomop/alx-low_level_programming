e25840924705e93d1ae7d02d3311fb2c77c963a7
Yomop <sunix1990@gmail.cmo>
2023-06-21 01:06:45 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_10x - prints alpha 10 times
+ */
+{
+	int ten;
+	char la;
+
+	for (ten = 0; ten <= 9; ten++)
+	{
+		for (la = 'a'; la <= 'z'; la++)
+			_putchar(la);
+		_putchar('\n');
+	}
+}
