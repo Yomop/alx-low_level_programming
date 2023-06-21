a129765d8e50748b8ab5681c1ae0c55d79ef044b
Yomop <sunix1990@gmail.cmo>
2023-06-21 06:43:51 -0700

    3. islower

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,6 +3,7 @@
 /**
  * print_alphabet_10x - prints alpha 10 times
  */
+void print_alphabet_x10(void);
 {
 	int ten;
 	char la;
--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,10 +5,12 @@
  * @C: the character
  * Return: 1 if letter is lowercase, 0 if not
  */
+
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
+	{
 		return (1);
-	else 
-		return (0);
+	}
+	return (0);
 }
