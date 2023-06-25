9e5c644e0cebe6f6575f7304539196e7166f193c
Yomop <sunix1990@gmail.cmo>
2023-06-25 02:48:05 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints a number in loercase
+ * followed by a new line, expect q and e
+ * return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		if (low != 'e' && low != 'q')
+			putchar(low);
+	}
+	putchar('\n');
+
+	return (0);
+}
