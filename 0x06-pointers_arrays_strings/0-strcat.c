0419c6cf2301059c7d1df4870a489a2b756ad879
Yomop <sunix1990@gmail.cmo>
2023-06-29 18:09:40 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,10 +2,11 @@
 
 /**
  * _strcat - concatenates two strings
- * @dest - input value
- * @src - input value
+ * @dest: input value
+ * @src: input value
  * Return: void
  */
+
 char *_strcat(char *dest, char *src);
 {
 	int i;
