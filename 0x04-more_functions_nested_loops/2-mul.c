60f32baa4ff1a3a8d95f4fe90cbcbea32f88b655
Yomop <sunix1990@gmail.cmo>
2023-07-02 17:40:55 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multipy two ints
+ * @a: first int
+ * @b: second int
+ * Return: multiplication of two ints
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
